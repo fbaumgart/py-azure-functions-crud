e8e8d2508dc1a80478c8a84e6829a71a4e7d36dd
fbaumgart <f.m.baumgart@hotmail.com>
2020-01-25 18:48:01 +0100

    First commit

---
+++ HttpTrigger/__init__.py
@@ -0,0 +1,30 @@
+import logging
+import pyodbc
+import json
+import azure.functions as func
+
+server = 'fmmb-sql.database.windows.net'
+database = 'People'
+username = 'fmmb'
+password = 'Aa1Bb2Cc3'
+driver= '{ODBC Driver 17 for SQL Server}'
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT * FROM PERSON")
+    row = cursor.fetchone()
+    json_list = list()
+    while row:
+        person_id = str(row[0])
+        person_first_name = str(row[1])
+        person_last_name = str(row[2])
+        person_phone_number = str(row[3])
+        single_record_json = {"id": person_id, "first name": person_first_name, "last name": person_last_name, "phone number": person_phone_number}
+        json_list.append(single_record_json)
+        response = json.dumps(json_list)
+        row = cursor.fetchone()
+    return func.HttpResponse(response)
